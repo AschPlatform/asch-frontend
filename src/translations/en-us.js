f66b5cff692758a69c5180e456a595562c675582
Xin Song <songxin1973@gmail.com>
2016-10-09 00:01:28 +0100

    Update English translation

--- src/translations/en-us.js
+++ src/translations/en-us.js
@@ -1,11 +1,175 @@
 if (!window.Translations) window.Translations = {};
 
 window.Translations['en-us'] = {
-  PREV_PAGE: '上一页',
-  NEXT_PAGE: '下一页',
+  //PREV_PAGE: '上一页',
+  //NEXT_PAGE: '下一页',
 
+  //DAPP_ICON: 'Icon',
+  //DAPP_DOWNLOAD: 'Download',
+  //DAPP_LIST: 'Applications',
+  //DAPP_INSTALL_LIST: 'Installed',
+  
+  // common
+  PREV_PAGE: 'Previous Page',
+  NEXT_PAGE: 'Next Page',
+
+  TOTAL: 'Total',
+  HEIGHT: 'Height',
+  DATE: 'Date',
+  PRODUCER: 'Producer',
+  TRANSACTIONS: 'Transaction',
+  AMOUNTS: 'Amount',
+  FEES: 'Fee',
+  REWARDS: 'Reward',
+  TYPE: 'Type',
+  SENDER: 'Sender',
+  RECIPIENT: 'Recipient',
+  SECOND_PASSWORD: 'Second Password',
+
+  // transaction type filter
+  TRS_TYPE_TRANSFER: 'Transfer',
+  TRS_TYPE_SECOND_PASSWORD: 'Second Password',
+  TRS_TYPE_DELEGATE: 'Delegate',
+  TRS_TYPE_VOTE: 'Vote',
+  TRS_TYPE_MULTISIGNATURE: 'Multiple Signature',
+  TRS_TYPE_DAPP: 'Register Application',
+  TRS_TYPE_DEPOSIT: 'Deposit',
+  TRS_TYPE_WITHDRAWAL: 'Withdraw',
+
+  // application
   DAPP_ICON: 'Icon',
+  DAPP_NAME: 'Name',
+  DAPP_DESCRIPTION: 'Description',
+  DAPP_CATEGORY: 'Type',
+  DAPP_SOURCE_CODE: 'Source Code',
   DAPP_DOWNLOAD: 'Download',
-  DAPP_LIST: 'Applications',
-  DAPP_INSTALL_LIST: 'Installed',
+  DAPP_LIST: 'Application List',
+  DAPP_INSTALL_LIST: 'Installed Application List',
+
+  // dapp category filter
+  DAPP_CATEGORY_COMMON: 'General',
+  DAPP_CATEGORY_BUSINESS: 'Business',
+  DAPP_CATEGORY_SOCIAL: 'Social',
+  DAPP_CATEGORY_EDUCATION: 'Education',
+  DAPP_CATEGORY_ENTERTAINMENT: 'Entertainment',
+  DAPP_CATEGORY_NEWS: 'News',
+  DAPP_CATEGORY_LIFE: 'Life',
+  DAPP_CATEGORY_UTILITIES: 'Utilities',
+  DAPP_CATEGORY_GAMES: 'Games',
+
+  // blockchain browser
+  LATEST_BLOCK: 'Latest Block',
+  INPUT_SEARCH_CONTENT: 'type the searching content',
+
+  // block forging
+  DELEGATE_INFO: 'Delegate Information',
+  DELEGATE_REGISTER: 'Register Delegate',
+  FORGING_ENABLE: 'Enabled',
+  FORGING_DISABLE: 'Disabled',
+  TOTAL_EARNINGS: 'Total Revenue',
+  RANKING: 'Ranking',
+  PRODUCTIVITY: 'Productivity',
+  APPROVAL: 'Approval Rate',
+  PRODUCED_BLOCKS: 'Produced Blocks',
+
+  // header
+  HOME: 'Home',
+  PERSONAL: 'My Account',
+  APPLICATIONS: 'Applications',
+  FORGING: 'Block Forging',
+  BLOCKS: 'Block Listing',
+  VOTE: 'Vote',
+  TRANSFER: 'Transfer',
+  PEERS: 'Peers',
+
+  // home
+  BALANCE: 'Balance',
+  LATEST_BLOCK_HEIGHT: 'Latest Block Height',
+  VERSION_INFO: 'Version',
+  MY_TRSACTIONS: 'My Transactions',
+
+  // login
+  INPUT_PASSWORD: 'Type the master password',
+  KEEP_SESSION: 'Keep Login',
+  LOGIN: 'Login',
+  NEW_ACCOUNT: 'New Account',
+  STEP: 'Step',
+  CREATE_MASTER_PASSWORD: 'Create Master Password',
+  NEW_PASSWORD: 'New Master Password',
+  NEW_PWD_TIP_1: 'System has already generated a new secure password, you need to re-type this password in the next step to confirm.',
+  NEXT_STEP: 'Next Step',
+  SAVE_PASSWORD: 'Save Master Password',
+  CONFIRM_PASSWORD: 'Confirm master password',
+  INPUT_PASSWORD_AGAIN: 'Please re-type your master password',
+  NEW_PWD_TIP_2: 'Make sure you have saved your master password in a safe place. If you lost it or forget it, your account will not be available and you will lost all your XAS.',
+  CONFIRM: 'Confirm',
+  CANCEL: 'Cancel',
+
+  // transfer/pay
+  SEND: 'Send',
+  PAY_TIP: 'Please make sure you send XAS to the correct address. This operation cannot be cancelled.',
+
+  // peers
+  PEER_LIST: 'Peer List',
+  OPERATING_SYSTEM: 'OS',
+  VERSION: 'Version',
+
+  // personal
+  ACCOUNT_INFO: 'Account Information',
+  QUIT: 'Quit',
+  BASIC_INFO: 'General Information',
+  ADDRESS: 'Address',
+  PUBLIC_KEY: 'Public Key',
+  ALREADY_SET_TPI: 'You have already set the second password.',
+  SET_SECOND_PASSWORD: 'Set the second password',
+  PASSWORD_RULE_TIP: 'Password must be from 8 to 16 alphanumeric characters',
+  INPUT_AGAIN: 'Enter Again',
+  PASSWORD: 'Password',
+  SUBMIT_SECOND_PASSWORD_TIP: 'Make sure you have saved your second password in a safe place. If you lost it, you will not be able to recover your assets in Asch system. You will be charged in XAS when you reset the second password.',
+  SUBMIT: 'Submit',
+  ALREADY_SET: 'Already Set',
+  NOT_SET: 'Not set yet',
+
+  // vote
+  DELETE: 'Delete',
+  DELEGATE_LIST: 'Delegate List',
+  VOTE_RECORD: 'Vote Record',
+  MY_VOTERS: 'My Voters',
+  DELEGATE: 'Delegate',
+  PRODUCED_NUMBER: 'Number of produced blocks',
+  USERNAME: 'User Name',
+  WEIGHT: 'Weight',
+  TOTAL_PEOPLES: 'Totally {{count}} people',
+
+  // model - account detail
+  ACCOUNT_DETAIL: 'Account Detail',
+
+  // model - delegate register
+  REGISTER_DELEGATE: 'Register as Delegate',
+  DELEGATE_NAME: 'Delegate\'s Name',
+  INPUT_DELEGATE_NAME: 'Please type the delegate name',
+  DELEGATE_NAME_RULE_TIP: 'Name must be at least 8 alphanumeric characters.',
+  REGISTER: 'Register',
+  NEED_PAY: 'Payment Required',
+
+  // model - block detail
+  BLOCK_DETAIL: 'Block Detail',
+  TIME: 'Time',
+  PREVIOUS_BLOCK: 'Previous Block',
+  TRANSACTIONS_COUNT: 'Transaction Count',
+  TOTAL_AMOUNTS: 'Total Amount',
+  PAYLOAD_HASH: 'Payload Hash',
+  PRODUCER_PUBKEY: 'Producer Public Key',
+
+  // model - transaction detail/dealinfo
+  TRANSACTION_INFO: 'Transaction Inforamtion',
+  CONFIRMATIONS: 'Confirmation Count',
+
+  // model - delete vote
+  DELETE_VOTE_TITLE: 'Delete vote for delegate',
+  DELETE_VOTE_TIP: 'You can delete vote for up to 33 delegates at a time.',
+
+  // model - vote
+  VOTE_TITLE: 'Vote for delegate',
+  VOTE_TIP: 'Please confirm your vote. You can choose up to 33 people in one vote.',  
 };
